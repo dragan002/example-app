96f2c75c94e49a464ffd75671a7385125cd20f47
dragan002 <draganvujic29@gmail.com>
2024-05-12 22:26:27 +0200

    Table notes fixed

--- database/migrations/2024_05_12_091512_create_notes_table.php
+++ database/migrations/2024_05_12_091512_create_notes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrainde();
             $table->string('title');
             $table->longText('text');
             $table->timestamps();
